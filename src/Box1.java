78d71417f9dfe34dbafa0de6cf8a6e6a5c8d1411
S2031851 <S2031851@S0085CG9454JMQ.student.HISD.ORG>
2024-04-17 13:08:58 -0500

    update

---
+++ src/Box1.java
@@ -0,0 +1,111 @@
+
+public class Box1 {
+	private String pic;
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private int dx;
+	private int dy;
+	private boolean movert;
+	private boolean movedn;
+	
+public Box1(){
+		
+		x=30;
+		y=20;
+		width=25;
+		movert=false;
+		movedn=false;
+		dx=0;
+		dy=0;
+	}
+	
+	public Box1(String s, int x1, int y1, int w, int h, int dx1, int dy1) {
+		pic=s;
+		x=x1;
+		y=y1;
+		width=w;
+		height=h;
+		dx=dx1;
+		dy=dy1;
+		movert=true;
+		movedn=true;
+	}
+	public String getPic() {
+		return pic;
+	}
+	public void setX(int c) {
+		x=c;
+	}
+	public void setY(int c) {
+		y=c;
+	}
+	public void setDx(int c) {
+		dx=c;
+	}
+	public void setDy(int c) {
+		dy=c;
+	}
+	public int getW() {
+		return width;
+	}
+	public int getH() {
+		return height;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getDX() {
+		return dx;
+	}
+	public int getDY() {
+		return dy;
+	}
+	public void clearDX() {
+		dx = 0;
+	}
+	public void clearDY() {
+		dy = 0;
+	}
+
+public void bounce1(int w1, int h1) {
+	this.x+=this.dx;
+	this.y+=this.dy;
+	if(this.x+this.width>w1) {
+		this.dx*=-1;
+		this.x=w1-this.width;
+	}
+	if(this.x<0) {
+		this.dx*=-1;
+		this.x=0;
+	}
+	if(this.y+this.height>h1) {
+		this.dy*=-1;
+		this.y=h1-this.height;
+	}
+	if(this.y<0) { 
+		this.dy*=-1;
+		this.y=0;
+	}
+	}
+	public void move(int w1, int h1) {
+		x+=dx;
+		y+=dy;
+		if(x+width>w1)
+			x=w1-width;
+		if(x<0)
+			x=0;
+		if(y+height>h1)
+			y=h1-height;
+		if(y<0)
+			y=0;
+	}
+	public boolean Collision(Box1 b)	{
+		return getX()+getW()>=b.getX()&&getX()<=b.getX()+b.getW()&&
+				getY()+getH()>=b.getY()&&getY()<=b.getY()+b.getH();
+	}
+}
---
+++ src/Game.java
@@ -0,0 +1,243 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+
+public class Game extends JPanel implements Runnable, KeyListener{
+	private BufferedImage back;
+	private Sound p;
+	private Sound l;
+	private int key;
+	private int gs;
+	private int gds;
+	private int zx;
+	private int kx;
+	private int gscore;
+	private int gscorex;
+	private int kscore;
+	private int kscorex;
+	private Box1 r1;//Blue Goalie
+	private Box1 r2;//Red Ball
+	private Box1 r3;//White Goal
+	private Box1 r4;//Grass
+	private Box1 r5;//zlatan
+	private Box1 r6;//ordament
+	
+	public Game() {
+		back=null;
+		p = new Sound();
+		l = new Sound();
+		new Thread(this).start();
+		this.addKeyListener(this);
+		r1=new Box1("grinch.png",250,260,75,75,0,0);
+		r2=new Box1("giftt.png",500,260,20,20,0,0);
+		r3=new Box1("bagg.png",75,100,200,350,0,0);
+		r4=new Box1("snowfield.jpg",0,0,800,600,0,0);
+		r5=new Box1("santaa.png",520,230,185,180,0,0);
+		r6=new Box1("ordd.png",1,1,100,100,3,3);
+		//455
+		//r1=new Box();
+		//r2=new Box(400,300,70,90,2,1);
+		key=-1;
+		gscore=0;
+		kscore = 0;
+		gscorex=1;
+		kscorex=1;
+	}
+
+	
+	public void run() {
+		try {
+			while(true) {
+				Thread.currentThread().sleep(5);
+				repaint();
+			}
+		}
+		catch(Exception e) {}
+	}
+	
+	public void paint (Graphics g)
+	{
+		Graphics2D twoDgraph = (Graphics2D)g;
+	//take a snap shop of the current screen and same it as an image
+	//that is the exact same width and height as the current screen
+		if (back==null) {
+			back =(BufferedImage) (createImage(getWidth(), getHeight()));
+				}
+
+	//create a graphics reference to the back ground image
+	//we will draw all changes on the background image
+		Graphics g2d = back.createGraphics();
+		
+		//this clears the old image, like an EtchASketch. If you see the old image when we learn motion, you deleted this line.
+		g2d.clearRect(0, 0, getSize().width, getSize().height); 
+		
+		
+		//START CODING GRAPHICS HERE
+		//sets the new color. Think of the computer picking up a pen
+		/*
+		g2d.setColor(Color.BLUE);
+		g2d.fillRect(r1.getX(),r1.getY() ,r1.getW(), r1.getH());
+		g2d.setColor(Color.RED);
+		g2d.fillRect(r2.getX(),r2.getY(), r2.getW(), r2.getH());
+		g2d.setColor(Color.white);
+		g2d.fillRect(r3.getX(),r3.getY(), r3.getW(), r3.getH());
+		*/
+		kx=400;
+		zx=-300;
+		g2d.drawImage(new ImageIcon(r4.getPic()).getImage(),r4.getX(),r4.getY(),r4.getW(),r4.getH(),this);
+		g2d.drawImage(new ImageIcon(r1.getPic()).getImage(),r1.getX(),r1.getY(),r1.getW(),r1.getH(),this);
+		g2d.drawImage(new ImageIcon(r2.getPic()).getImage(),r2.getX(),r2.getY(),r2.getW(),r2.getH(),this);
+		g2d.drawImage(new ImageIcon(r5.getPic()).getImage(),r5.getX(),r5.getY(),r5.getW(),r5.getH(),this);
+		g2d.drawImage(new ImageIcon(r3.getPic()).getImage(),r3.getX(),r3.getY(),r3.getW(),r3.getH(),this);
+		g2d.drawImage(new ImageIcon(r6.getPic()).getImage(),r6.getX(),r6.getY(),r6.getW(),r6.getH(),this);
+		g2d.setColor(Color.green);
+		g2d.setFont(new Font ("Impact", Font.PLAIN, 36));
+		g2d.drawString("Presents Stolen: "+String.valueOf(gscore), 50, 50);
+		g2d.drawString("December 25th",kx, 500);
+		g2d.setColor(Color.red);
+		g2d.drawString("Gifts in Bag: "+String.valueOf(kscore), kx, 50);
+		g2d.drawString("Christmas",200,500);
+		g2d.drawString("Zlatan's Score: "+String.valueOf(kscore), zx, 50);
+		
+		
+		//400
+		g2d.setColor(Color.red);
+		g2d.setFont(new Font ("chiller", Font.BOLD, 100));
+		if (kscore>= 15 ) {
+			///l.playmusic("loss.wav");
+			g2d.drawString("You LOSE",235, 300);
+		}
+		g2d.setColor(Color.green);
+		g2d.setFont(new Font ("Impact", Font.PLAIN, 100));
+		if (gscore>= 10 && kscore<15) {
+			g2d.drawString("You WIN",235, 300);
+		}
+		move(getSize().width, getSize().height);
+		if(r2.Collision(r3))
+		{
+			resetBall();
+			kscore+=kscorex;
+		}
+		if(r2.Collision(r1)) {
+			resetBall();
+			gscore+=gscorex;
+			//p.playmusic("clapzz.wav");
+		}
+		
+		// create an open rectangle	
+		// create a filled Rect		
+		//This line tells the program to draw everything above. If you delete this, nothing will show up.
+		twoDgraph.drawImage(back, 0, 0, null);
+	}
+	public void move(int w1,int h1) {
+		r1.move(w1,h1);
+		r2.move(w1,h1);
+		r6.bounce1(w1,h1);
+	}
+	public void keyTyped(KeyEvent e) {
+		
+	}
+	public void keyPressed(KeyEvent e) {
+		key=e.getKeyCode();
+
+		if(key==74) {//cheatcode
+			gscorex++;
+		}
+		if(key==75) {//cheatcodekicker
+			r5.setX(455);
+			//kx=-400;
+			//zx=400;
+			kscorex++;
+			//else {
+				//kx=400;
+				//zx=-300;
+			//;}
+		}
+		gs=-3;
+		gds=3;
+		//if(key==66) {
+			//clap();
+		//}
+		if(key==38)//up
+			r1.setDy(gs);
+		if(key==40)//down
+			r1.setDy(gds);
+		//if(key==37)//left
+			//r1.setDx(-3);
+		//if(key==39)//right
+		//	r1.setDx(3);
+		if(key==32) {//Space
+			p.playmusic("hohoho.wav");
+			double dy=5*Math.random()-2.5;
+			resetBall();
+			r2.setDx(-3);
+			r2.setDy((int)dy);
+		}
+		if(key==86) {//resetscore
+			gscore=0;
+			kscore=0;
+		}
+		if(key==67){//resetgoalie
+			resetGoalie();
+		}
+		if(key==49){//topshot
+			p.playmusic("hohoho.wav");
+			r2.setDx(-3);
+			r2.setDy(-2);
+		}
+		if(key==50) {//upshot
+			p.playmusic("hohoho.wav");
+			r2.setDx(-3);
+			r2.setDy(-1);
+		}
+		if(key==51) {//downshot
+			p.playmusic("hohoho.wav");
+			r2.setDx(-3);
+			r2.setDy(1);
+		}
+		if(key==52) {//botttomshot
+			p.playmusic("hohoho.wav");
+			r2.setDx(-3);
+			r2.setDy(2);
+		}
+		if(key==53) {//middleshot
+			p.playmusic("hohoho.wav");
+			r2.setDx(-3);
+			r2.setDy(0);	
+		}
+	}
+	public void keyReleased(KeyEvent e) {
+		key=e.getKeyCode();
+		if(key==38)
+			r1.clearDY();
+		if(key==40)
+			r1.clearDY();
+		//if(key==37)
+			//r1.clearDX();
+		//if(key==39)
+			//r1.clearDX();
+	}
+	public void resetBall() {
+		r2.setDx(0);
+		r2.setDy(0);
+		r2.setX(500);
+		r2.setY(260);
+	}
+	public void resetGoalie() {
+		r1.setDx(0);
+		r1.setDy(0);
+		r1.setX(250);
+		r1.setY(260);
+	}
+	public void zlatan() {
+		
+	}
+	public void clap() {
+		var sound=true;
+		if (gscore == 10 && sound) {
+			  p.playmusic("mario.wav");
+			  sound=false;
+			}
+	}
+}
